78394409eae346e158040aa21d1c370ed8458de1
Jose Cisneros <JoseC@sendsations.com>
2022-11-10 17:47:41 -0700

    initial commit

---
+++ components/nav/TopHeader.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <b-navbar toggleable="lg" type="dark" variant="info">
+            <b-navbar-brand href="#">C&C</b-navbar-brand>
+
+            <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+
+            <b-collapse id="nav-collapse" is-nav>
+                <!-- Right aligned nav items -->
+                <b-navbar-nav class="ml-auto">
+                    <b-nav-item href="#">RSVP</b-nav-item>             
+                    <b-nav-item-dropdown text="ABOUT" right>
+                        <b-dropdown-item href="#">GIFTS</b-dropdown-item>
+                        <b-dropdown-item href="#">OUR STORY</b-dropdown-item>
+                        <b-dropdown-item href="#">Q + A</b-dropdown-item>
+                        <b-dropdown-item href="#">TRAVEL</b-dropdown-item>
+                    </b-nav-item-dropdown>
+                </b-navbar-nav>
+            </b-collapse>
+        </b-navbar>
+    </div>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,48 @@
+<template>
+  <section>
+    <TopHeader />
+    <div class="relative flex items-top justify-center min-h-screen bg-gray-100 sm:items-center sm:pt-0">
+      <div class="max-w-4xl mx-auto sm:px-6 lg:px-8">
+        <div class="mt-8 bg-white overflow-hidden shadow sm:rounded-lg p-6">
+          <div>
+            <b-carousel id="carousel-1" v-model="slide" :interval="4000" controls indicators background="#ababab"
+              img-width="1024" img-height="480" style="text-shadow: 1px 1px 2px #333;" @sliding-start="onSlideStart"
+              @sliding-end="onSlideEnd">
+              <!-- Text slides with image -->
+              <b-carousel-slide caption="First slide" text="Nulla vitae elit libero, a pharetra augue mollis interdum."
+                img-src="https://picsum.photos/1024/480/?image=52"></b-carousel-slide>
+
+              <!-- Slides with custom text -->
+              <b-carousel-slide img-src="https://picsum.photos/1024/480/?image=54">
+                <h1>Hello world!</h1>
+              </b-carousel-slide>
+
+              <!-- Slides with image only -->
+              <b-carousel-slide img-src="https://picsum.photos/1024/480/?image=58"></b-carousel-slide>
+
+              <!-- Slides with img slot -->
+              <!-- Note the classes .d-block and .img-fluid to prevent browser default image alignment -->
+              <b-carousel-slide>
+                <template #img>
+                  <img class="d-block img-fluid w-100" width="1024" height="480"
+                    src="https://picsum.photos/1024/480/?image=55" alt="image slot">
+                </template>
+              </b-carousel-slide>
+            </b-carousel>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+import TopHeader from '@/components/nav/TopHeader.vue'
+
+export default {
+  name: 'IndexPage',
+  components: {
+    TopHeader
+  }
+}
+</script>
