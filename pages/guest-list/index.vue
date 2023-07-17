b41a2e4474d529a501e8e387e87c08dcc2280652
Jose Cisneros <josecis0216@gmail.com>
2023-07-17 17:19:48 -0600

    guest list changes

---
+++ pages/guest-list/index.vue
@@ -0,0 +1,137 @@
+<template>
+    <section>
+        <TopHeader />
+        <div class="sectionBody">
+            <b-container>
+                <b-row>
+                    <b-col>
+                        <b-card style="max-width: 40rem; margin: 200px auto 0px auto">
+                            <b-form v-if="show" @submit.prevent="onSubmit" @reset="onReset">
+                                <b-form-group id="input-group-1" label="Guest Name:" label-for="input-1">
+                                    <b-form-input id="input-1" v-model="form.fullName" placeholder="Enter guest name"
+                                        required></b-form-input>
+                                </b-form-group>
+
+                                <b-button pill variant="outline-success" @click="showAddGuests" v-show="!showGuests">Add
+                                    Guest</b-button>
+
+                                <b-form-group v-for="(guest, k) in form.guests" :key="k" v-show="showGuests">
+                                    <b-form-input type="text" class="form-control" placeholder="Guest Name"
+                                        v-model="guest.name"></b-form-input>
+                                    <span>
+                                        <b-button pill variant="outline-danger" @click="remove(k)"
+                                            v-show="k || (!k && form.guests.length > 1)">Remove</b-button>
+                                        <b-button pill variant="outline-success" @click="add(k)"
+                                            v-show="k == form.guests.length - 1">Add
+                                            Guest</b-button>
+                                    </span>
+                                </b-form-group>
+
+
+                                <b-form-group id="input-group-check" v-slot="{ ariaDescribedby }">
+                                    <b-form-checkbox-group v-model="form.brideOrGroom" id="checkboxes-2"
+                                        :aria-describedby="ariaDescribedby">
+                                        <b-form-checkbox value="bride">Guest of the bride</b-form-checkbox>
+                                        <b-form-checkbox value="groom">Guest of the groom</b-form-checkbox>
+                                    </b-form-checkbox-group>
+                                </b-form-group>
+
+                                <b-card-text></b-card-text>
+
+                                <b-button type="submit" variant="primary">Submit</b-button>
+                                <b-button type="reset" variant="danger">Reset</b-button>
+                            </b-form>
+                        </b-card>
+                    </b-col>
+                </b-row>
+            </b-container>
+        </div>
+
+        <bottom-footer />
+
+        <!-- <div>
+        <b-card class="mt-3" header="Form Data Result">
+          <pre class="m-0">{{ form }}</pre>
+        </b-card>
+      </div> -->
+    </section>
+</template>
+  
+<script>
+import TopHeader from '@/components/nav/TopHeader.vue'
+import BottomFooter from '@/components/nav/BottomFooter.vue'
+
+export default {
+    components: {
+        TopHeader,
+        BottomFooter
+    },
+    data() {
+        return {
+            form: {
+                fullName: '',
+                brideOrGroom: [],
+                guests: [{
+                    name: ''
+                }],
+            },
+            show: true,
+            showGuests: false
+        }
+    },
+    methods: {
+        showAddGuests() {
+            this.showGuests = true
+        },
+
+        add() {
+            this.form.guests.push({
+                name: ''
+            })
+            console.log(this.form.guests)
+        },
+
+        remove(index) {
+            this.form.guests.splice(index, 1)
+            console.log(this.form.guests)
+        },
+
+        onSubmit(event) {
+            console.log('SUCCESS! Form was submitted')
+            this.$store.dispatch('rsvp', this.form).then(() => {
+                alert('Thank you for responding, can\'t wait to celebrate with you!')
+                this.$router.push('/')
+            })
+        },
+        onReset(event) {
+            event.preventDefault()
+            // Reset our form values
+            this.form.fullName = ''
+            this.form.brideOrGroom = []
+            // Trick to reset/clear native browser form validation state
+            this.show = false
+            this.$nextTick(() => {
+                this.show = true
+            })
+        },
+    },
+}
+</script>
+  
+<style scoped>
+.sectionBody {
+    /* background-image: url('https://images.pexels.com/photos/313707/pexels-photo-313707.jpeg?auto=compress&cs=tinysrgb&w=960&h=450&dpr=2'); */
+    background-image: url('~/static/wedding-photo-rsvp.jpeg');
+    background-size: cover;
+    background-repeat: no-repeat;
+    min-height: 860px;
+}
+
+@media only screen and (max-width: 500px) {
+    .sectionBody {
+        background-position-y: 1rem;
+        background-position-x: -30rem;
+    }
+}
+</style>
+  
--- store/index.js
+++ store/index.js
@@ -2,14 +2,21 @@ export default {
     state() {
         return {
             requests: [],
+            guestList: []
         };
     },
     mutations: {
         addRequest(state, payload) {
             state.requests.push(payload);
         },
+        addGuest(state, payload) {
+            state.guestList.push(payload);
+        },
         setRequests(state, payload) {
             state.requests = payload;
+        },
+        setGuestList(state, payload) {
+            state.guestList = payload;
         }
     },
     actions: {
@@ -20,7 +27,31 @@ export default {
                 guests: payload.guests
                 //   message: payload.message,
             };
-            const response = await fetch(`https://clarissa-carlos-default-rtdb.firebaseio.com/requests.json`, { // fetch(`https://vue-practice-88f8e-default-rtdb.firebaseio.com/requests/requests.json`
+            const response = await fetch(`https://moy-ashley-default-rtdb.firebaseio.com/requests.json`, {
+                method: 'POST',
+                body: JSON.stringify(requestData)
+            })
+
+            const responseData = await response.json();
+
+            if (!response.ok) {
+                const error = new Error(responseData.message || 'failed to send request.');
+                throw error;
+            }
+
+            requestData.id = responseData.name;
+            requestData.coachId = payload.coachId;
+
+            context.commit('addRequest', requestData);
+        },
+        async addToGuestList(context, payload) {
+            const requestData = {
+                fullName: payload.fullName,
+                brideOrGroom: payload.brideOrGroom,
+                guests: payload.guests
+                //   message: payload.message,
+            };
+            const response = await fetch(`https://moy-ashley-default-rtdb.firebaseio.com/requests.json`, {
                 method: 'POST',
                 body: JSON.stringify(requestData)
             })
@@ -38,7 +69,7 @@ export default {
             context.commit('addRequest', requestData);
         },
         async loadRequests(context) {
-            const response = await fetch(`https://clarissa-carlos-default-rtdb.firebaseio.com/requests.json`);  // fetch(`https://vue-practice-88f8e-default-rtdb.firebaseio.com/requests/requests.json`)
+            const response = await fetch(`https://moy-ashley-default-rtdb.firebaseio.com/requests.json`);
 
             const responseData = await response.json();
 
@@ -60,6 +91,26 @@ export default {
             }
             context.commit('setRequests', requests);
         },
+        async loadGuestList(context) {
+            const response = await fetch(`https://moy-ashley-default-rtdb.firebaseio.com/guest-list.json`);
+
+            const responseData = await response.json();
+
+            if (!responseData.ok) {
+                const error = new Error(responseData.message || 'Failed to fetch data.');
+            }
+
+            const guestList = [];
+            for (const key in responseData) {
+                const requestData = {
+                    id: key,
+                    fullName: responseData[key].fullName,
+                    guests: responseData[key].guests
+                };
+                guestList.push(requestData);
+            }
+            context.commit('setGuestList', guestList);
+        }
     },
     getters: {
         requests(state) {  // _, _2, rootGetters
@@ -69,5 +120,8 @@ export default {
         hasRequests(_, getters) {
             return getters.requests && getters.requests.length > 0;
         },
+        guestList(state) {
+            return state.guestList.guests;
+        }
     },
 };
