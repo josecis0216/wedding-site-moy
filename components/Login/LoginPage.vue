9cc27bf7e7f47a81be47a0c7c540ad220a38086a
Jose Cisneros <josecis0216@gmail.com>
2023-07-17 17:19:26 -0600

    updated for guest list request and new repo

---
+++ components/Login/LoginPage.vue
@@ -0,0 +1,35 @@
+<template>
+    <b-container>
+        <b-row>
+            <b-col>
+                <b-card>
+                    <b-form @submit.prevent="onSubmit" @reset="onReset">
+                        <b-form-group id="input-group-1" label="Your Full Name:" label-for="input-1">
+                            <b-form-input id="inputName" placeholder="Enter full name" required></b-form-input>
+                        </b-form-group>
+                    </b-form>
+                </b-card>
+            </b-col>
+        </b-row>
+    </b-container>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            form: {
+                fullName: ""
+            }
+        }
+    },
+    methods: {
+        onSubmit(event) {
+
+        },
+        onReset(event) {
+            this.form.fullName = ""
+        }
+    }
+}
+</script>
